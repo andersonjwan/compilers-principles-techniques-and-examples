91587df5610c49b5647950ab0165a2c1ef1a1eb3
andersonjwan <andersonjwan@outlook.com>
2020-07-15 23:49:25 -0700

    implemented string and token tables alongside keyword and identifier analysis

--- chapter-2/section-6/examples/ex-2.6.c
+++ chapter-2/section-6/examples/ex-2.6.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* terminal(s) */
 enum terminals {NUM = 256, ID, TRUE, FALSE};
@@ -16,12 +18,37 @@ typedef struct Tokens {
   char *lexeme; // reserved words and/or identifiers
 } Token;
 
-Token words[1000];        // string table for keywords and identifiers
-static int next_word = 0; // next available slot
+char *words[1000];        // string table for keywords and identifiers
+int next_word = 0;        // next available word
 
-void reserve(Token t) {
-  words[next_word] = t;
+Token *tokens[1000];       // token table for all tokens
+int next_token = 0;       // next available token
+
+void reserve(Token *t, char *string) {
+  words[next_word] = string;
   ++next_word;
+
+  tokens[next_token] = t;
+  ++next_token;
+}
+
+Token * get(char *string) {
+  for(int i = 0; i < next_word; ++i) {
+    if(strcmp(string, words[i]) == 0) {
+      /* lexeme exist in table */
+      return tokens[next_token];
+    }
+  }
+
+  return NULL;
+}
+
+void put(char *string, Token *t) {
+  words[next_word] = string;
+  ++next_word;
+
+  tokens[next_token] = t;
+  ++next_token;
 }
 
 Token scan(void) {
@@ -50,12 +77,12 @@ Token scan(void) {
     while(peek >= 48 && peek <= 57); // while peek is a digit
 
     /* create and return new token */
-    Token t;
-    t.tag = NUM;
-    t.line = line;
-    t.value = v;
+    Token *t = (Token *) malloc(sizeof(Token));
+    t->tag = NUM;
+    t->line = line;
+    t->value = v;
 
-    return t;
+    return *t;
   }
 
   if((peek >= 65 && peek <= 90) || (peek >= 97 && peek <= 122)) {
@@ -70,11 +97,11 @@ Token scan(void) {
 
     do {
       /* allocate more memory if needed */
-      if(next_char >= size) {
-        char *temp_buffer = realloc(buffer, size + 2);
-        size = size + 1;
+      if(size <= next_char) {
+        size = size + 2;
+        char *temp_buffer = realloc(buffer, size);
 
-        if(temp_buffer != NULL) {
+        if(temp_buffer == NULL) {
           fprintf(stderr, "Memory Allocation Error: Unable to reallocate memory.\n");
           exit(2);
         }
@@ -93,29 +120,78 @@ Token scan(void) {
     /* append null terminating character */
     buffer[next_char] = '\0';
 
-    /* create and return new token */
-    Token t;
-    t.tag = ID;
-    t.line = line;
-    t.lexeme = buffer;
+    /* check existence of token */
+    Token *t = (Token *) get(buffer);
 
-    // TODO: implement hastable
-    // TODO: build hashtable that maps lexemes to tokens
-    // TODO: map new Token into hashtable
+    if(t != NULL) {
+      return *t;
+    }
+
+    /* create and return new token */
+    t = (Token *) malloc(sizeof(Token));
+
+    t->tag = ID;
+    t->line = line;
+    t->lexeme = buffer;
+
+    put(buffer, t);
+    return *t;
   }
+
+  Token t;
+  t.tag = peek;
+  peek = ' '; // reset peek to blank-space
+
+  return t;
 }
 
 int main(void) {
   /* setup reserved words */
-  Token keyword;
+  Token keyword_1, keyword_2;
 
-  keyword.tag = TRUE;
-  keyword.lexeme = "true";
-  reserve(keyword);
+  keyword_1.tag = TRUE;
+  keyword_1.lexeme = "true";
+  reserve(&keyword_1, keyword_1.lexeme);
 
-  keyword.tag = FALSE;
-  keyword.lexeme = "false";
-  reserve(keyword);
+  keyword_2.tag = FALSE;
+  keyword_2.lexeme = "false";
+  reserve(&keyword_2, keyword_2.lexeme);
+
+  while(1) {
+    scan();
+
+    for(int i = 0; i < next_token; ++i) {
+      /* print Token tag */
+      printf("Token Tag: ");
+      if(tokens[i]->tag == NUM) {
+        printf("NUM\n");
+      }
+      else if(tokens[i]->tag == ID) {
+        printf("ID\n");
+      }
+      else if(tokens[i]->tag == TRUE) {
+        printf("TRUE\n");
+      }
+      else if(tokens[i]->tag == FALSE) {
+        printf("FALSE\n");
+      }
+      else {
+        printf("NO MATCHING TAG\n");
+      }
+
+      if(tokens[i]->tag == NUM) {
+        printf("Token Value: %d\n", tokens[i]->value);
+      }
+      else if(tokens[i]->tag == ID || tokens[i]->tag == TRUE || tokens[i]->tag == FALSE) {
+        printf("Token Lexeme: %s\n", tokens[i]->lexeme);
+      }
+      else {
+        printf("NO MATCHING VALUE / LEXEME");
+      }
+
+      printf("----------\n");
+    }
+  }
 
   return 0;
 }
